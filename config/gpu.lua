3e3e1883b36adfbe18e94e873b81582d06669d86
jintao_xiao <xiaojintao@outlook.com>
2026-03-12 20:13:55 +0800

    fix(config): handle missing battery and register format-window-title event

--- config/gpu.lua
+++ config/gpu.lua
@@ -5,7 +5,8 @@ Config.webgpu_force_fallback_adapter = false
 
 ---switch to low power mode when battery is low
 ---@diagnostic disable-next-line: undefined-field
-local battery_charge = require("wezterm").battery_info()[1].state_of_charge
+local battery_info = require("wezterm").battery_info()
+local battery_charge = battery_info[1] and battery_info[1].state_of_charge or 1.0
 if battery_charge < 0.35 then
   Config.webgpu_power_preference = "LowPower"
 else
--- wezterm.lua
+++ wezterm.lua
@@ -2,6 +2,7 @@ local Config = require("utils.class.config"):new()
 
 require "events.update-status"
 require "events.format-tab-title"
+require "events.format-window-title"
 require "events.new-tab-button-click"
 require "events.augment-command-palette"
 
